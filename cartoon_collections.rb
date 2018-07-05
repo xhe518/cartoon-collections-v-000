752d78fa33d96e4212fd5d694ca0787f4be1cca5
Sid He <xhe518@gmail.com>
2018-07-05 19:36:07 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ end
 def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
   if foods.include?(cheese_types) == true
-    foods.find("cheddar")
+    foods.find(cheese_types)
   else
     nil
   end
